37b8825642352d68e434549b04c55df5d46332e7
mifflinbalmer <mifflin.balmer@protonmail.com>
2022-08-07 12:34:26 -0400

    fgfg

--- api/upload.js
+++ api/upload.js
@@ -1,12 +1,12 @@
 const fileUpload = require('express-fileUpload');
 
-const createUpload = (req, res) = > {
+const createUpload = (req, res) => {
   if ( !req.files || Object.keys(req.files).length === 0 ) {
     return res.status(400).send('No files were uploaded.');
   }
 
   for ( const file in req.files ) {
-    let uploadPath = __dirname + '/media/files' + file.name;
+    let uploadPath = `${ process.env.VOLUME_PATH }/assets/files/${ file.name }`;
 
     file.mv(uploadPath, (err) => {
       if ( err ) {
@@ -15,6 +15,7 @@ const createUpload = (req, res) = > {
 
       res.send('File uploaded.');
     });
+  };
 };
 
 exports.createUpload = createUpload;
